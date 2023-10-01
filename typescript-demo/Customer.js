5509f91a07543fa421ddd430a522674c908d6efd
Linxerz <pruittj2000@Yahoo.com>
2023-10-01 18:09:19 -0500

    Made a Demo class and created TS config file

---
+++ typescript-demo/Customer.js
@@ -0,0 +1,31 @@
+"use strict";
+var Customer = /** @class */ (function () {
+    function Customer(firstName, lastName) {
+        this._firstName = firstName;
+        this._lastName = lastName;
+    }
+    Object.defineProperty(Customer.prototype, "firstName", {
+        get: function () {
+            return this._firstName;
+        },
+        set: function (value) {
+            this._firstName = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Customer.prototype, "lastName", {
+        get: function () {
+            return this._lastName;
+        },
+        set: function (value) {
+            this._lastName = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Customer;
+}());
+var myCustomer = new Customer("John", "Doe");
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
